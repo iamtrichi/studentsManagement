9ca227d9a09fb058d505d0dbc10f032cc7208a85
iamtrichi <oussema.trichi@vistaprint.com>
2021-12-19 18:24:54 +0100

    courses issues

--- src/main/webapp/app/entities/subjects/subjects.model.ts
+++ src/main/webapp/app/entities/subjects/subjects.model.ts
@@ -2,11 +2,15 @@ export interface ISubjects {
   id?: string;
   subject?: string;
   keywords?: string;
-  courses?: any[] | null;
+  courses?: any[] | null | undefined;
 }
 
 export class Subjects implements ISubjects {
-  constructor(public id?: string, public subject?: string, public keywords?: string, public courses: any[] | null = []) {}
+  constructor(public id?: string, public subject?: string, public keywords?: string, public courses?: any[] | null | undefined) {
+    if (!courses) {
+      this.courses = [];
+    }
+  }
 }
 
 export function getSubjectsIdentifier(subjects: ISubjects): string | undefined {
